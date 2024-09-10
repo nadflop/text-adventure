5e0cfcdd7be47f3686641cd7ceed2af6a2a7840d
nadflop <31370689+nadflop@users.noreply.github.com>
2024-09-10 17:29:32 +0900

    Added conversations

--- header/noun.h
+++ header/noun.h
@@ -3,5 +3,6 @@
 
 extern OBJECT *getVisible(const char *intention, const char *noun);
 extern OBJECT *getPossession(OBJECT *from, const char *verb, const char *noun);
+extern OBJECT *getTopic(const char *noun);
 
 #endif
---
+++ header/talk.h
@@ -0,0 +1,7 @@
+#ifndef TALK_H
+#define TALK_H
+
+extern bool executeTalk(void);
+extern bool executeTalkTo(void);
+
+#endif
